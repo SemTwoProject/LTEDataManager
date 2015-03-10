cac4ed1a7554bb529ab3a6df346fe461405f80d7
Flynn25 <D14125331@mydit.ie>
2015-03-10 21:05:36 +0000

    Fixed extra errors

--- src/main/webapp/js/networkmanagement.js
+++ src/main/webapp/js/networkmanagement.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 			if ($(this).attr("value") == "totalfaults") 
 			{			
 				$("#dates").show();
-<<<<<<< HEAD
+
 				$("#phonemodeldropdown").prop("disabled", true);
 				$('#datatable').empty();
 				var table = $('<tr><th>IMSI</th><th>Total Failures</th><th>Total Duration</th></tr>');				
@@ -43,11 +43,11 @@ $("#submit").click(function()
 		{
 	var startdate = $('#startdate').data('date');
 	var enddate = $('#enddate').data('date');
-	
+
 	alert(startdate);
 	alert(enddate);
-	
-	
+
+
 	$('#datatable').empty();
 	var table = $('<tr><th>IMSI</th><th>Total Failures</th><th>Total Duration</th></tr>');				
 	$('#datatable').append(table);
@@ -73,82 +73,4 @@ $("#submit").click(function()
 			}});			
 	}
 		});
-=======
-				$("#phonemodelsearch").hide();
-				$('#datatable').empty();
-				var table = $('<tr><th>IMSI</th><th>Total Failures</th><th>Total Duration</th></tr>');				
-				$('#datatable').append(table);			
-			}
-			if ($(this).attr("value") == "query2") 
-			{				
-				$("#dates").hide();
-				$("#phonemodelsearch").show();
-				$('#datatable').empty();
-				var table = $('<tr><th>Phone Model</th><th>Event ID and Cause Code combination</th><th>Number of Occurences</th></tr>');				
-				$('#datatable').append(table);
-			}
-			if ($(this).attr("value") == "query3") 
-			{		
-				$("#dates").show();
-				$("#phonemodelsearch").hide();
-				$('#datatable').empty();
-				var table = $('<tr><th>Rank</th><th>Market ID</th><th>Operator ID</th><th>Call ID</th></tr>');				
-				$('#datatable').append(table);
-			}
-			if ($(this).attr("value") == "query4") 
-			{		
-				$("#dates").show();
-				$("#phonemodelsearch").hide();
-				$('#datatable').empty();
-				var table = $('<tr><th>Rank</th><th>IMSI</th></tr>');				
-				$('#datatable').append(table);
-			}
-		});
-	}).change();
-});
 
-
-$("#submit").click(function()
-{
-	var startdate = $('#startdate').data('date');
-	var enddate = $('#enddate').data('date');
-	
-	if ($("#querydropdown").attr("value") == "totalfaults") 
-	{	
-		 $.ajax({
-		        type: 'POST',
-		        url: "http://localhost:8080/LTEManager/rest/fault/totalfaults",
-		        dataType: "json", 
-		        data: {"startdate": startdate, "enddate": enddate},
-		        success:function(response)
-		        {
-		        	$.each(response, function(i, item) 
-		        	{
-		        		alert(item);
-		        	});          	
-		        }});
-					
-	}
-});
-
-
-//$.post("/rest/fault/totalfaults",
-//		{
-//			startdate: startdate,
-//			enddate: enddate
-//		},
-//		 function(response)
-//	        {
-//	        	var $tr;
-//	        	$.each(response, function(i, item) 
-//	        	{
-//	        		alert(response);
-//	        		$tr = "";
-//	        		$tr = $('<tr>').append(
-//	                $('<td>').text(item.get(0)),
-//	        	    $('<td>').text(item.get(1)),
-//	        	    $('<td>').text(item.get(2)));
-//	            	$('#usertable').append($tr);
-//	        	});  
-//	        });
->>>>>>> branch 'master' of https://github.com/SemTwoProject/LTEDataManager
