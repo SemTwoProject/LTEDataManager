6da6c6604563c9d470393219ba3b4e70464ea5b5
AlanKavo92 <alankavo92@hotmail.com>
2015-04-09 14:40:57 +0100

    Added FAULT and Market Operator JUNIT TESTS

--- src/test/java/junit/TestEntities.java
+++ src/test/java/junit/TestEntities.java
@@ -1,19 +1,27 @@
 package junit;
 
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.entity.CellHier;
+import com.entity.EventCause;
+import com.entity.Failure;
+import com.entity.Fault;
+import com.entity.MarketOperator;
 import com.entity.MccMnc;
 import com.entity.UE;
 import com.entity.User;
 
 public class TestEntities
 {
-
+	Fault f;
+	MarketOperator mo;
 	MccMnc m;
 	UE ue;
 	User u;
@@ -22,6 +30,8 @@ public class TestEntities
 	@Before
 	public void setUp() throws Exception
 	{
+		f = new Fault();
+		mo = new MarketOperator();
 		m = new MccMnc();
 		ue = new UE();
 		u = new User();
@@ -30,9 +40,45 @@ public class TestEntities
 	@After
 	public void tearDown() throws Exception
 	{
+		f = null;
+		mo = null;
 		m = null;
 		ue = null;
 		u = null;
+	}
+	
+	@Test
+	public void testFault()
+	{
+		f.setId(1);
+		f.setImsi(1234l);
+		f.setDate(new Date());
+		f.setNe("11B");
+		f.setDuration(1000);
+		assertEquals((int)f.getId(), 1);
+		assertEquals((long)f.getImsi(), 1234l);
+		assertEquals(f.getDate(), new Date());
+		assertEquals(f.getNe(), "11B");
+		assertEquals((int)f.getDuration(), 1000);
+		f.setFailure(new Failure(0, "Emergency"));
+		f.setTac(new UE());
+		f.setCell(new CellHier(4, 123l, 124l, 125l));
+		f.setMccid(new MccMnc(238, "Denmark", 1, "TDC-DK"));
+		f.setEventCause(new EventCause(0, 4097, "RRC CONN-SETUP"));
+		assertEquals((int)f.getFailure().getfailure(), 0);
+		assertEquals((int)f.getCell().getCellId(), 4);
+		assertEquals((int)f.getMccid().getMccId(), 238);
+		assertEquals((int)f.getEventCause().getEventId(), 4097);
+		
+	}
+	
+	@Test
+	public void testMarketOperator()
+	{
+		mo.setMccId(238);
+		mo.setMncId(1);
+		assertEquals((int)mo.getMccId(), 238);
+		assertEquals((int)mo.getMncId(), 1);
 	}
 	
 	@Test
