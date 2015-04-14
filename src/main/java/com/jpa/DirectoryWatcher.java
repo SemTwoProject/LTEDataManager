6e24beaca791feec4a3e30bfb48f02aa979be7fd
andrew-phelan <seridis@hotmail.com>
2015-04-14 17:35:13 +0100

    Update DirectoryWatcher.java
    Set the timeout timer

--- src/main/java/com/jpa/DirectoryWatcher.java
+++ src/main/java/com/jpa/DirectoryWatcher.java
@@ -11,6 +11,7 @@ import java.nio.file.Path;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.Date;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -25,6 +26,7 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.ejb.Timeout;
 import javax.ejb.Timer;
+import javax.ejb.TimerConfig;
 import javax.ejb.TimerService;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
@@ -46,7 +48,7 @@ public class DirectoryWatcher {
 	private TimerService timerService;
 
 	@PostConstruct
-	public void atStartUp() {
+	public void atStartUp() throws InterruptedException{
 		FileSystem fileSystem = FileSystems.getDefault();
 		Path path = fileSystem.getPath("c:\\excel\\");
 		try {
@@ -55,11 +57,12 @@ public class DirectoryWatcher {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		timerService.createIntervalTimer(new Date(), new Date().getTime(), new TimerConfig());
 	}
-
+	
 	@Lock(LockType.READ)
-	@Schedule(minute = "*/1", hour = "*", persistent = false)
-	public void execute() {
+	@Schedule(hour="*", minute="*/1", persistent = false)
+	public void execute() throws InterruptedException{
 		String fileName;
 		String extension;
 		try {
@@ -67,12 +70,12 @@ public class DirectoryWatcher {
 			for (WatchEvent<?> event : key.pollEvents()) {
 				fileName = "" + event.context();
 				extension = FilenameUtils.getExtension(fileName.toString());
+				fileName.replaceAll("\\s+", "");
 				if (event.kind() == OVERFLOW)
 					continue;
 				else if (extension.equals("xls")) {
-					System.out.println("File " + fileName
-							+ " importing.");
-					dao.callAll(fileName.replaceAll("\\s+", ""));
+					System.out.println("File " + fileName + " importing.");
+					dao.callAll(fileName);
 				} else {
 					System.out.println("File " + fileName
 							+ " should be of type .xls");
